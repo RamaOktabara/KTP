0861f01e04c0362a4232951de27ac8c0c15a571d
RamaOktabara <100754126+RamaOktabara@users.noreply.github.com>
2022-04-18 02:25:07 +0700

    Add files via upload

---
+++ KTP.java
@@ -0,0 +1,8 @@
+public class KTP{
+    public static void main(String[] args) {
+        Scanner inputan= new Scanner();
+        System.out.println("masukan nama= ");
+        inputan=nama;
+        
+    }
+}
